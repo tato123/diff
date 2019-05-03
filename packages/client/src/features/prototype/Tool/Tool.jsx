5b8cf6bc9f5552753219baa57710a94d194f3c6b
Jonathan Fontanez <fontanezj1@gmail.com>
2019-05-02 20:05:06 -0400

    added more to the editor

--- packages/client/src/features/prototype/Tool/Tool.jsx
+++ packages/client/src/features/prototype/Tool/Tool.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { Col, Button, InputNumber } from "antd";
+import { Col, Button, InputNumber, Checkbox, Input, Select } from "antd";
+import { SketchPicker } from "react-color";
+import Draggable from "react-draggable";
+
+const Option = Select.Option;
 
 const Container = styled.div`
   width: 400px;
@@ -9,10 +13,16 @@ const Container = styled.div`
   border: 3px solid #000;
   border-radius: 16px;
   position: absolute;
-  z-index: 10001;
+  z-index: 2;
   bottom: 16px;
   right: 16px;
   padding: 16px;
+
+  &:hover {
+    .handle {
+      opacity: 1;
+    }
+  }
 `;
 
 const Row = styled.div`
@@ -60,6 +70,40 @@ const HR = styled.hr`
   margin: 16px 0 0 0;
 `;
 
+const ColorPreview = styled.div`
+  width: 22px;
+  height: 16px;
+  border: 1px solid #dadce0;
+  display: inline-block;
+`;
+
+InputNumber.Custom = styled(InputNumber)`
+  border-radius: 0px !important;
+  border: none !important;
+  border-bottom: 1px solid #dadce0 !important;
+  margin-left: 8px !important;
+`;
+
+Input.Custom = styled(Input)`
+  border-radius: 0px !important;
+  border: none !important;
+  border-bottom: 1px solid #dadce0 !important;
+  margin-left: 8px !important;
+`;
+
+const DragHandle = styled.div`
+  width: 100%;
+  height: 16px;
+  background: rgba(0, 0, 0, 0.1);
+  opacity: 0.1;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  margin-top: -8px;
+  margin-bottom: 8px;
+  cursor: move;
+  transition: all 250ms ease-in;
+`;
+
 const Field = ({ title, children }) => (
   <ToolField>
     <label className="title">{title}:</label>
@@ -67,54 +111,88 @@ const Field = ({ title, children }) => (
   </ToolField>
 );
 
+const ColorPicker = ({ color = "transparent" }) => (
+  <>
+    <ColorPreview color={color} />
+    <Input.Custom style={{ width: 100 }} />
+  </>
+);
+
 const Tool = ({ state }) => (
-  <Container>
-    <Row>
-      <div>
-        <span className="title">Diff</span>
-        <span className="sub-title">.cta_button_hero</span>
-      </div>
-      <Col>X</Col>
-    </Row>
-    <HR />
-    <Row mt>
-      <Button>Move</Button>
-      <Button>Rotate</Button>
-    </Row>
-    <Row mt>
-      <div>
-        <span>W</span>
-        <InputNumber min={1} max={1000000000000000000000} defaultValue={3} />
-      </div>
-      <div>
-        <span>H</span>
-        <InputNumber min={1} max={1000000000000000000000} defaultValue={3} />
-      </div>
-    </Row>
-    <Row mt col2>
-      <Field title="Background Color">
-        <label>123</label>
-      </Field>
-      <Field title="Text Color">
-        <label>123</label>
-      </Field>
-    </Row>
-    <Row mt>
-      <Field title="Font">
-        <label>123</label>
-      </Field>
-    </Row>
-    <Row mt>
-      <Field title="Border Radius">
-        <label>123</label>
-      </Field>
-    </Row>
-    <Row mt>
-      <Field title="Text">
-        <label>123</label>
-      </Field>
-    </Row>
-  </Container>
+  <Draggable handle=".handle">
+    <Container>
+      <DragHandle className="handle" />
+      <Row>
+        <div style={{ flex: 1 }}>
+          <span className="title">Diff</span>
+          <span className="sub-title">.cta_button_hero</span>
+        </div>
+        <Col style={{ justifyContent: "flex-end" }}>
+          <Button
+            icon="close-circle"
+            style={{ border: "none", fontSize: 20 }}
+          />
+        </Col>
+      </Row>
+      <HR />
+      <Row mt>
+        <Button>Move</Button>
+        <Button>Rotate</Button>
+      </Row>
+      <Row mt col2>
+        <div>
+          <span>W</span>
+          <InputNumber.Custom min={1} max={100000} defaultValue={3} />
+          <Checkbox>auto</Checkbox>
+        </div>
+        <div>
+          <span>H</span>
+          <InputNumber.Custom min={1} max={100000} defaultValue={3} />
+          <Checkbox>auto</Checkbox>
+        </div>
+      </Row>
+      <Row mt col2>
+        <Field title="Background Color">
+          <ColorPicker />
+        </Field>
+        <Field title="Text Color">
+          <ColorPicker />
+        </Field>
+      </Row>
+      <Row mt>
+        <Field title="Font">
+          <InputNumber.Custom
+            min={1}
+            max={100000}
+            style={{ width: 55, margin: 0 }}
+          />
+          <Select defaultValue="100">
+            <Option value="100">100</Option>
+            <Option value="300">300</Option>
+            <Option value="500">500</Option>
+          </Select>
+          <Select defaultValue="lucy" style={{ width: 120 }}>
+            <Option value="jack">Jack</Option>
+            <Option value="lucy">Lucy</Option>
+            <Option value="disabled" disabled>
+              Disabled
+            </Option>
+            <Option value="Yiminghe">yiminghe</Option>
+          </Select>
+        </Field>
+      </Row>
+      <Row mt>
+        <Field title="Border Radius">
+          <label>123</label>
+        </Field>
+      </Row>
+      <Row mt>
+        <Field title="Text">
+          <label>123</label>
+        </Field>
+      </Row>
+    </Container>
+  </Draggable>
 );
 
 Tool.defaultProps = {
